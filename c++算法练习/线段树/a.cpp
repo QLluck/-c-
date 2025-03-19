6ec00360a85e087199c9a271f3208bc82658cb37
QLluck <2929798027@qq.com>
2025-03-19 21:31:45 +0800

    2025年03月19日21:31:43

---
+++ c++算法练习/线段树/a.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std ;
+#define ll long long 
+const int N =1e5+7 ;
+struct node
+{
+     ll l,r,sum,lazzy ;
+}t[4*N];
+ll arr[N]={0};
+
+void build(ll p , ll l ,ll r)
+{
+      node temp = {l,r,arr[l],0};
+      t[p] = temp ;
+      if(l==r)return ;
+      ll m= (l+r)>>1;
+      build(p<<1,l,m);
+      build(p<<1|1,m+1,r);
+   t[p].sum = t[p<<1].sum + t[p<<1|1].sum ;
+
+}
+void pushdown(ll p)
+{
+     if(t[p].lazzy)
+     {
+        t[p<<1].sum += t[p].lazzy*(t[p<<1].r - t[p<<1].l +1);
+        t[p<<1|1].sum += t[p].lazzy*(t[p<<1|1].r - t[p<<1|1].l +1);
+        t[p<<1].lazzy += t[p].lazzy;
+        t[p<<1|1].lazzy += t[p].lazzy;
+        t[p].lazzy =0 ;
+     }
+}
+void update(ll p ,ll l,ll r,ll w)
+{
+    if(l<=t[p].l&&t[p].r<=r)
+    {
+          t[p].sum += w*(t[p].r - t[p].l +1);
+          t[p].lazzy+=w;
+          return ;
+    }
+    ll m = (t[p].l+t[p].r)>>1;
+    pushdown(p);
+    if(l<=m) update(p<<1,l,r,w);
+    if(m<r) update(p<<1|1,l,r,w);
+    t[p].sum = t[p<<1].sum +t[p<<1|1].sum ;
+}
+ll query(ll p ,ll l, ll r)
+{
+    if(l<=t[p].l&&t[p].r<=r)
+    {
+         return t[p].sum ;
+    }
+    ll m = (t[p].l+t[p].r)>>1;
+    pushdown(p);
+    ll sum = 0 ;
+    if(l<=m)sum+= query(p<<1,l,r);
+    if(m<r) sum+=query(p<<1|1,l,r);
+    return sum ;
+    
+}
+
+void solve()
+{
+   int n,m;
+   cin>>n>>m;
+   for(int i =1; i<=n ;i++)
+   {
+     cin>>arr[i];
+   }
+   build(1,1,n);
+   for(int i =1; i<=m ;i++)
+   { ll x ,l,r,k;
+      cin>>x;
+    switch(x)
+    {   
+
+
+        case 1 :
+        cin>>l>>r>>k;
+        update(1,l,r,k);
+        break;
+        case 2:
+
+          cin>>l>>r;
+        
+         
+          cout<<query(1,l,r)<<'\n';
+          break;
+        
+          
+
+            
+    }
+    
+   }
+   
+}
+int main()
+{
+     ios::sync_with_stdio(0),cout.tie(0),cin.tie(0);
+     solve();
+}
---
+++ c++算法练习/线段树/b.cpp
@@ -0,0 +1,130 @@
+#include<bits/stdc++.h>
+using namespace std ;
+const int N =1e5+7 ;
+#define ll long long 
+ll n,q,m;
+struct node
+{
+    ll l,r ,sum,la,lax ;
+
+}t[N*4];
+ll arr[N]={0};
+void pushup(ll p)
+{
+      t[p].sum =( t[p<<1].sum +t[p<<1|1].sum ) %m;
+}
+
+void pushdown(ll p)
+{
+      if(t[p].la)
+      {
+          t[p<<1].sum +=  (t[p].la *(t[p<<1].r -t[p<<1].l+1))%m;
+          t[p<<1|1].sum +=  (t[p].la *(t[p<<1|1].r -t[p<<1|1].l+1))%m;
+          t[p<<1].la += t[p].la ;
+          t[p<<1|1].la += t[p].la ;
+          t[p].la = 0 ;
+          
+      }
+}
+void build(ll p,ll l,ll r )
+{
+     node temp = {l,r,arr[l],0,0};
+     t[p]=temp ;
+     cout<<l<<' '<<r<<' '<<arr[l]<<' '<<t[p].sum<<'\n';
+     if(l==r)return ;
+     ll m = (l+r)>>1;
+     build(p<<1,l,m);
+     build(p<<1|1,m+1,r);
+     pushup(p);
+
+}
+void update(ll p,ll l,ll r,ll w)
+{
+      if(l<=t[p].l&&t[p].r<=r)
+    {
+        t[p].sum+=w*(t[p].r-t[p].l+1) %m;
+        t[p].la +=w ;
+        return ;
+    }
+    pushdown(p);
+    ll m =(t[p].r+t[p].l)>>1;
+    if(l<=m) update(p<<1,l,r,w);
+    if(m<r) update(p<<1|1,l,r,w);
+    pushup(p);
+
+}
+void updatex(ll p,ll l,ll r ,ll w)
+{   pushdown(p);
+    if(l<=t[p].l&&t[p].r<=r)
+    {
+        t[p].sum =w*t[p].sum %m;
+       
+    }
+    if(t[p].l==t[p].r)return ;
+    ll m =(t[p].r+t[p].l)>>1;
+    if(l<=m) updatex(p<<1,l,r,w);
+    if(m<r) updatex(p<<1|1,l,r,w);
+    
+
+    
+}   
+ll query(ll p,ll l,ll r)
+{    cout<<t[p].sum<<' '<<p<<'\n';
+    // if(l<=t[p].l&&t[p].r<=r)
+    // {
+    //     return t[p].sum%m;
+    // }
+    if(t[p].r==t[p].l)return  t[p].sum;
+    pushdown(p);
+    ll m =(t[p].r+t[p].l)>>1;
+    ll sum = 0 ;
+    if(l<=m) sum+=query(p<<1,l,r) %m ;
+    if(m<r) sum+=query(p<<1|1,l,r)%m ;
+    
+    return sum %m;
+}
+void print()
+    {
+        for(int i=1 ; i<=n ;i++)cout<<query(1,i,i)<<' ';
+        cout<<'\n';
+    }
+
+void solve()
+{
+    cin>>n>>q>>m;
+    for(int i=1 ;i<=n; i++)cin>>arr[i];
+    for(int i =1; i<=n ;i++)cout<<arr[i]<<' ';
+    cout<<'\n';
+    build(1,1,n);
+    query(1,1,n);
+    //print();
+    for(int i =1 ;i<=q ; i++)
+    {
+         ll  x,l,r,k;
+         cin>>x;
+         if(x==1)
+         {  cin>>l>>r>>k;
+            updatex(1,l,r,k);
+         }
+         else if(x==2)
+         {
+             cin>>l>>r>>k;
+             update(1,l,r,k);
+
+            
+         }
+         else if(x==3)
+         {
+             cin>>l>>r;
+             cout<<query(1,l,r)<<'\n';
+         }
+         print();
+    }
+}
+int main()
+{
+     ios::sync_with_stdio(0),cout.tie(0),cin.tie(0);
+     solve();
+}
+
+    
