ce3b84294c6cc21614b4bfad01215ce83d3a0831
QLluck <2929798027@qq.com>
2025-05-07 23:34:50 +0800

    2025年5月7日23:34:48

--- cf/2091F.cpp
+++ cf/2091F.cpp
@@ -1,93 +1,108 @@
-#include<bits/stdc++.h>
-using namespace std ;
-#define ll long long 
-const int P =998244353;
-const int N =2e3+7;
-ll pix[N][N]={0};
-char arr[N][N]={0};
-ll sum[N][N]={0};
-int n,m;
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll P = 998244353;
+const int N = 2e3 + 7;
+ll pix[N][N] = {0};
+char arr[N][N] = {0};
+ll sum[N][N] = {0};
+int n, m;
 void print()
 {
-    for(int i =1; i<=n;i++)
+    cout << "------------pix--------" << '\n';
+    for (int i = 1; i <= n; i++)
     {
-         for(int j=1; j<=m;j++)
-         {
-            cout<<sum[i][j]<<' ';
-            
-         }
-         cout<<'\n';
+        for (int j = 1; j <= m; j++)
+        {
+            cout << pix[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    cout << "-----sum-----" << '\n';
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cout << sum[i][j] << ' ';
+        }
+        cout << '\n';
     }
 }
 void solve()
-{  int d;
-    cin>>n>>m>>d;
-    double r1 = sqrt(d*d-1);
-    int r = round(r1);
-    cout<<"r "<<r<<'\n';
-    for(int i =1; i<=n;i++)
+{
+    int d;
+    cin >> n >> m >> d;
+    double r1 = sqrt(d * d - 1);
+    // cout << r1 << '\n';
+    r1 += 0.5;
+    int rn = round(r1);
+    // cout<<"r "<<rn<<'\n';
+    for (int i = 1; i <= n; i++)
     {
-         for(int j =1; j<=m;j++)
-         {
-             cin>>arr[i][j];
-             sum[i][j]=0;
-             pix[i][j]=0;
-             
-         }
-    }
-    for(int i =1; i<=m;i++)
-     { pix[n+1][i]=0;
-       if(arr[n][i]=='X') sum[n][i]=1;
-     }
-    for(int i =n ;i>=1 ;i--)
-    {
-
-       // for(int j = 1; j<=m;j++)pix[i][j]=sum[i][j]+pix[i][j-1];
-        for(int j =1; j<=m;j++)
+        for (int j = 1; j <= m; j++)
         {
-             if(arr[i][j]=='#')continue;
-              int l = max(1,j-r);
-              int r = min(m,j+r);//待定
-              
-              sum[i][j] += pix[i+1][r]-pix[i+1][l-1];
-              sum[i][j]%=P;
-             
+            cin >> arr[i][j];
+            sum[i][j] = 0;
+            pix[i][j] = 0;
         }
-        cout<<"1-------------"<<'\n';
-      print();
-   
-
-        for(int j = 1; j<=m;j++)
-         { pix[i][j]=sum[i][j]+pix[i][j-1];
-            pix[i][j]%=P;
-         }
-        for(int j =1;j<=m;j++)
-        {    if(arr[i][j]=='#')continue;
-            int l1= max(1,j-d);
-            int r1 = min(m,j+d);
-            sum[i][j] += pix[i][r1]-pix[i][l1-1]-sum[i][j] ;
-            sum[i][j]%=P;
-        }
-        cout<<"2-------------"<<'\n';
-       print();
-        cout<<"---------"<<'\n';
-         
-
     }
-    ll ans= 0 ;
-    print();
-    for(int i=1; i<=m;i++)
+    for (int i = 1; i <= m; i++)
     {
-         ans= (ans +sum[1][i])%P;
+        pix[n + 1][i] = 0;
+        if (arr[n][i] == 'X')
+            sum[n][i] = 1;
     }
-    cout<<ans<<'\n';
-     
+    for (int i = n; i >= 1; i--)
+    {
+
+        // for(int j = 1; j<=m;j++)pix[i][j]=sum[i][j]+pix[i][j-1];
+        // cout << "1-------------" << '\n';
+        for (int j = 1; j <= m; j++)
+        {
+            if (arr[i][j] == '#')
+                continue;
+            int l = max(1, j - rn + 1);
+            int r = min(m, j + rn - 1); // 待定
+
+            // cout<<j<<' ' << l << ' ' << r << ' '<<rn<<'\n';
+            sum[i][j] = (sum[i][j] + pix[i + 1][r] - pix[i + 1][l - 1] + P) % P;
+        }
+
+        // print();
+
+        for (int j = 1; j <= m; j++)
+        {
+            pix[i][j] = (sum[i][j] + pix[i][j - 1]) % P;
+        }
+        // cout << "2-------------" << '\n';
+        for (int j = 1; j <= m; j++)
+        {
+            if (arr[i][j] == '#')
+                continue;
+            int l1 = max(1, j - d);
+            int r1 = min(m, j + d);
+            sum[i][j] = (pix[i][r1] - pix[i][l1 - 1] + P) % P;
+        }
+        for (int j = 1; j <= m; j++)
+        {
+            pix[i][j] = (sum[i][j] + pix[i][j - 1] + P) % P;
+        }
+        // print();
+        //  cout<<"---------"<<'\n';
+    }
+    ll ans = 0;
+    // print();
+    for (int i = 1; i <= m; i++)
+    {
+        ans = (ans + sum[1][i]) % P;
+    }
+    cout << ans << '\n';
 }
 int main()
 {
-    ios::sync_with_stdio(0),cout.tie(0),cin.tie(0);
+    ios::sync_with_stdio(0), cout.tie(0), cin.tie(0);
     int t;
-    cin>>t;
-    while(t--)
-    solve();
+    cin >> t;
+    while (t--)
+        solve();
 }
--- cf/tempCodeRunnerFile.cpp
+++ cf/tempCodeRunnerFile.cpp
@@ -1,2 +1,108 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll P = 998244353;
+const int N = 2e3 + 7;
+ll pix[N][N] = {0};
+char arr[N][N] = {0};
+ll sum[N][N] = {0};
+int n, m;
+void print()
+{
+    cout << "------------pix--------" << '\n';
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cout << pix[i][j] << ' ';
         }
+        cout << '\n';
+    }
+    cout << "-----sum-----" << '\n';
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cout << sum[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
+void solve()
+{
+    int d;
+    cin >> n >> m >> d;
+    double r1 = sqrt(d * d - 1);
+    // cout << r1 << '\n';
+    r1 += 0.5;
+    int rn = round(r1);
+    // cout<<"r "<<rn<<'\n';
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cin >> arr[i][j];
+            sum[i][j] = 0;
+            pix[i][j] = 0;
+        }
+    }
+    for (int i = 1; i <= m; i++)
+    {
+        pix[n + 1][i] = 0;
+        if (arr[n][i] == 'X')
+            sum[n][i] = 1;
+    }
+    for (int i = n; i >= 1; i--)
+    {
+
+        // for(int j = 1; j<=m;j++)pix[i][j]=sum[i][j]+pix[i][j-1];
+        // cout << "1-------------" << '\n';
+        for (int j = 1; j <= m; j++)
+        {
+            if (arr[i][j] == '#')
+                continue;
+            int l = max(1, j - rn + 1);
+            int r = min(m, j + rn - 1); // 待定
+
+            // cout<<j<<' ' << l << ' ' << r << ' '<<rn<<'\n';
+            sum[i][j] = (sum[i][j] + pix[i + 1][r] - pix[i + 1][l - 1] + P) % P;
+        }
+
+        // print();
+
+        for (int j = 1; j <= m; j++)
+        {
+            pix[i][j] = (sum[i][j] + pix[i][j - 1]) % P;
+        }
+        // cout << "2-------------" << '\n';
+        for (int j = 1; j <= m; j++)
+        {
+            if (arr[i][j] == '#')
+                continue;
+            int l1 = max(1, j - d);
+            int r1 = min(m, j + d);
+            sum[i][j] = (pix[i][r1] - pix[i][l1 - 1] + P) % P;
+        }
+        for (int j = 1; j <= m; j++)
+        {
+            pix[i][j] = (sum[i][j] + pix[i][j - 1] + P) % P;
+        }
+        // print();
+        //  cout<<"---------"<<'\n';
+    }
+    ll ans = 0;
+    // print();
+    for (int i = 1; i <= m; i++)
+    {
+        ans = (ans + sum[1][i]) % P;
+    }
+    cout << ans << '\n';
+}
+int main()
+{
+    ios::sync_with_stdio(0), cout.tie(0), cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
