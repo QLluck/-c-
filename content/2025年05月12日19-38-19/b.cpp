30671d5117ea5518cdc321027a7e12cd257f6a88
QLluck <2929798027@qq.com>
2025-05-14 21:21:47 +0800

    2025年05月14日21:21:44

--- content/2025年05月12日19-38-19/b.cpp
+++ content/2025年05月12日19-38-19/b.cpp
@@ -1,43 +1,79 @@
 #include<bits/stdc++.h>
-const int N =5e5+7;
+const int N =1e6+7;
 using namespace std ;
 #define ll long long 
-
-
+int arr[N]={0};
+int me[N]={0};
+struct node
+{
+    int num ,it;
+};
+node b[N]={0};
+bool cnt(node a,node b)
+{
+     if(a.num<b.num)return 1;
+     else if(a.num>b.num)return 0;
+     else 
+     {
+         return a.it<b.it;
+     }
+}
 void solve()
 {
 
-    string s ;
-    cin>>s;
-  
-    int l =0;
-    int r =1e3-2 ;
-    while(r<=1e6-1)
-    { 
-        int l1 =0 ;
-        int r1 = r;
-        int num= 0;
-        while(s[l1]==s[r1])
-        {
-            num++;
-            l1++;
-            r1++;
+   int n;
+   cin>>n;
+   for(int i=1; i<=n;i++)
+   {
+      cin>>arr[i];
+      b[i].num=arr[i];
+      b[i].it=i;
+   }
+   sort(b+1,b+n+1,cnt);
+   ll l = n&1? n/2+1 : n/2;
 
-        }
-        if(num>=1e3)
-        {
-            cout<<"NO"<<'\n';
-            return;
-        }
-        r++;
-
-
+   ll ans= 0 ;
+   for(int len =1; len<=n; )
+   {    
+    
+      if(me[len])
+    {
+        len++;
+        ans++;
+        continue;
     }
-    cout<<"YES"<<'\n';
+    for(int i =1; i<=n;i++)
+{
+       
     
-     
+       
+         ll l1 = min(i,b[i].it);
+        ll r1 = max(b[i].it,i);
+        while((l1/len * len  >= l1 || l1/len * len + len < r1 )&&len<=n )
+        {  len++;      
+        }
+        if(len>n)break;       
+        cout<<l1<< ' '<<r1<<' '<<len<<'\n';
+       
     
+  if(i==n)
+  { ans++;
+    
+    for(int j=1; j<=n+1;j++)
+    {
+         if(j*len>n)
+         {len++;
+           break;
+          }
+         me[j*len]=1;
+    }
    
+  }
+
+   }
+}
+
+cout<<ans<<'\n';
 
 }
 
