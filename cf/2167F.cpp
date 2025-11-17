bd14c976a92d586ec72f499ec819491abe4f4fa7
QLluck <2929798027@qq.com>
2025-11-17 21:46:29 +0800

    2025年11月17日21:46:27

---
+++ cf/2167F.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+#define PII pair<int, int>
+#define fi first
+#define se second
+const int N = 2e5 + 7;
+vector<int> t[N];
+// vector<int> t2[N] ;
+int d[N] = {0};
+int dn[N] = {0};
+map<PII, int> mp;
+queue<PII> que;
+int n, k;
+void bfs()
+{
+    // map<PII, int> me;
+
+    map<int, int> me;
+    while (que.size())
+    {
+        int u = que.front().se;
+        int sum = que.front().fi;
+        d[u] = -2;
+        que.pop();
+        for (int i = 0; i < t[u].size(); i++)
+        {
+            int v = t[u][i];
+            if (d[v] == -2)
+                continue;
+            mp[{u, v}] = n - sum;
+            mp[{v, u}] = dn[u] + 1;
+
+            dn[v] += dn[u] + 1;
+            if (d[v] == -1)
+                continue;
+            d[v] = -1;
+            que.push({sum + 1, v});
+        }
+    }
+}
+void solve()
+{
+
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++)
+    {
+        t[i].clear();
+        d[i] = 0;
+        dn[i] = 0;
+    }
+    mp.clear();
+    for (int i = 1; i <= n - 1; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        d[u]++;
+        d[v]++;
+        t[u].push_back(v);
+        t[v].push_back(u);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (d[i] == 1)
+            que.push({1, i});
+    }
+    bfs();
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int sum = 0;
+        int f = 0;
+        for (int j = 0; j < t[i].size(); j++)
+        {
+            int v = t[i][j];
+            int num = mp[{i, v}];
+            if (num >= (k - 1))
+            {
+                f = 1;
+                sum += (n - num - 1) * num;
+            }
+            cout << i << ' ' << v << ' ' << num << ' ' << sum << '\n';
+        }
+        if (f)
+            sum++;
+        ans += sum;
+    }
+    cout << ans << endl;
+}
+signed main()
+{
+    ios::sync_with_stdio(0), cout.tie(0), cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
