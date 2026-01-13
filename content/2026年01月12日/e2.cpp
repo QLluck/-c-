2eef068a856cc120dbdc92d0480ea5411b8e0fe5
QLluck <2929798027@qq.com>
2026-01-13 22:27:59 +0800

    2026年01月13日22:27:58

--- content/2026年01月12日/e2.cpp
+++ content/2026年01月12日/e2.cpp
@@ -12,6 +12,7 @@ struct node
 node o[N];
 int isn[N];
 vector<int> e[N];
+vector<int> e2[N];
 int ans[N],it;
 int ans1[N], it1;
 map<PII, int> mp;
@@ -20,28 +21,20 @@ stack<int> sta;
 int dfn[N], low[N];
 int cnt=0, cntb = 0;
 vector<int> ya[N];
-map<int, int> yad;
+int yad[N];
 PII dc[N];
 map<int, int> me1;
 map<int, int> me3;
-int dfs(int s)
+int dfs(int u)
 {
-    if(me1[s])return dc[s].se;
-    me1[s] = 1;
-    for (int i = 0; i <ya[s].size();i++)
+    if(me1[u])return dc[u].fi;
+    me1[u] = 1;
+    for (int i = 0; i <e2[u].size();i++)
     {
-        int u = ya[s][i];
-        for (int j = 0; j < e[u].size();j++)
-        {
-            int v = e[u][j];
-           if( !(s==yad[v]) )
-           {
-               dc[s].se+= dfs(yad[v]);
-           }
-        }
+        int v = e2[u][i];
+        if(!me1[v])dc[u].fi+=dfs(v);
     }
-   
-    return dc[s].se;
+    return dc[u].fi;
 }
 void tarjan(int u)
 {
@@ -69,6 +62,7 @@ void tarjan(int u)
            sta.pop();
            me[node] = 0;
            ya[cntb].push_back(node);
+           yad[node]=cntb;
 
        } while (node != u);
    }
@@ -78,7 +72,7 @@ void bfs(int s)
     queue<int> que;
     que.push(s);
     map<int, int> me;
-    
+    me[s]=1;
     while(que.size())
     {
         int u = que.front();
@@ -86,14 +80,19 @@ void bfs(int s)
         for (int i = 0; i < e[u].size();i++)
         {
             int v = e[u][i];
-            if(me[v]||isn[v])
+            if(me[v])
                 continue;
+                me[v]=1;
             ans1[++it1] = mp[{u, v}];
             isn[v] = 2;
             que.push(v);
         }
     }
 }
+ bool so(node a,node b)
+ {
+    return a.num<b.num;
+ }
 void solve()
 {
     int n, m;
@@ -105,7 +104,7 @@ void solve()
     me3.clear();
     cnt = 0, cntb = 0;
     for (int i = 1; i <= n;i++)
-        isn[i] = 0, e[i].clear(),dfn[i]=0,low[i]=0,ya[i].clear();
+        isn[i] = 0, e[i].clear(),dfn[i]=0,low[i]=0,ya[i].clear(),yad[i]=0;
         for (int i = 1; i <= m; i++)
         {
             o[i].id = i;
@@ -138,13 +137,28 @@ void solve()
     for (int i = 1; i <= n;i++)
        if(!dfn[i]&&me3[i]) tarjan(i);
     for (int i = 1; i <= cntb;i++)
-        dc[i].fi = ya[i].size(),dc[i].se;
+        dc[i].fi = ya[i].size(),dc[i].se=i;
+    map<PII,int>me4;
+    for(int i=1;i<=cntb;i++)
+    {
+        for(int j=0;j<ya[i].size();j++)
+        {
+             int u = ya[i][j];
+             for(int h =0 ;h<e[u].size();h++)
+             {
+                int v = yad[e[u][h]];
+                if(i==v || me4[{i,v}])continue;
+                me4[{i,v}]=1;
+                e2[i].push_back(v);
+             }
+        }
+    }
         for (int i = 1; i <= cntb; i++)
         {
-            if (me1[i])
+            if (!me1[i])
                 dfs(i);
         }
-        sort(dc, dc + cntb + 1);
+        sort(dc+1, dc + cntb + 1);
         for (int i = 1;i<=n;i++)
         {
                 if(isn[i]==2)
@@ -166,6 +180,7 @@ void solve()
             }
         }
         map<int, int> me2;
+        sort(o+1,o+m+1,so);
         for (int i = it1; i >= 1; i--)
         {
             // ans[++it] = ans1[i];
@@ -173,13 +188,19 @@ void solve()
         }
         for (int i = 1; i <= m; i++)
         {
-            if (me2[i] == 0 && o[i].num == 3)
-                ans[++it] = i;
+            if(o[i].num==2)
+            {
+                isn[o[i].l]=max(o[i].x,isn[o[i].l]);
+                isn[o[i].r]=max(o[i].y,isn[o[i].y]);
+               
+            }
+            else if (me2[i] == 0 && o[i].num == 3)
+                ans[++it] = o[i].id;
         }
       
         for (int i = it1;i>=1; i--)
           {  ans[++it] = ans1[i];
-              me2[ans1[i]] = 1;
+              
           }
           for (int i = 1; i <= m;i++)if(o[i].num==4)
                   ans[++it] = i;
@@ -187,7 +208,7 @@ void solve()
           for (int i = 1; i <= n;i++)
               sum += isn[i];
           cout << sum << '\n';
-          for (int i = 1; i <= m;i++)
+          for (int i = 1; i <= it;i++)
               cout << ans[i] << ' ';
           cout << '\n';
         //   for (int i = 1; i <= n;i++)
